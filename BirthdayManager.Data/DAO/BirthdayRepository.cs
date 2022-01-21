bcb0618e885553ea3d1361bc97798a9a093ffba8
David Tigre Moraes <david.tigre@lambda3.com.br>
2022-01-21 17:35:04 -0300

    Creating Repository

--- BirthdayManager.Data/DAO/BirthdayRepository.cs
+++ BirthdayManager.Data/DAO/BirthdayRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BirthdayManager.Data.Context;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,15 @@ namespace BirthdayManager.Data.DAO
 {
     internal class BirthdayRepository
     {
+        private MongoBirthdayDbContext _context;
+
+        public BirthdayRepository(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetSection("MongoSettings:ConnectionStrings").ToString();
+            var databaseName = configuration.GetSection("MongoSettings:DatabaseName").ToString();
+
+            _context = new MongoBirthdayDbContext(connectionString, databaseName);
+        }
+
     }
 }
